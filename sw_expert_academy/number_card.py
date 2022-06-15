986bc17fece11a5ebdb84a5b61532623f9f983d8
im-shung <seoyoung1113@gmail.com>
2022-06-15 11:11:10 +0900

    :recycle: 숫자 카드 문제 리팩토링

--- sw_expert_academy/number_card.py
+++ sw_expert_academy/number_card.py
@@ -2,24 +2,18 @@
 # 파이썬 SW문제해결 기본 - LIST1 8차시 
 
 T = int(input())
-result = list()
 
 for test_case in range(1, T + 1):
     N = int(input())
     arr = list(map(int,input()))
     
-    mid = [0,0,0,0,0,0,0,0,0,0]
+    # 카드가 0 - 9 까지만 존재하니까 0부터 9까지를 담을 수 있는 크기가 10인 리스트를 만든다.
+    mid = [0] * 10
     for a in arr:
-        mid[a]+=1
+        mid[a] += 1
         
     max = 0
-    for idx in range(len(mid)):
-        if mid[idx] > mid[max]:
-            max = idx
-        elif mid[idx] == mid[max]:
-            if idx > max:
-                max = idx
-    result.append(str(max)+" "+str(mid[max]))
-
-for idx in range(len(result)):
-    print("#"+str(idx+1),result[idx])
+    for i in range(len(mid)):
+        if mid[i] >= mid[max]:
+            max = i
+    print("#"+str(test_case),max,mid[max])
